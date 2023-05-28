c10e01e76138e1f0805b1c8901604903d15dd238
Ilya <b.ilya.98@mail.ru>
2023-05-28 13:15:02 +0300

    fixed comment entity

--- src/main/java/ru/skypro/homework/entity/Comment.java
+++ src/main/java/ru/skypro/homework/entity/Comment.java
@@ -1,10 +1,13 @@
 package ru.skypro.homework.entity;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.time.LocalDateTime;
-import java.util.Objects;
 
 @Entity
+@Data
+@Table(name = "comment")
 public class Comment {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
@@ -17,98 +20,4 @@ public class Comment {
     private int pk;
     private String text;
 
-    public Comment(int author, String authorImage, String authorFirstName, LocalDateTime createdAt, int pk, String text) {
-        this.author = author;
-        this.authorImage = authorImage;
-        this.authorFirstName = authorFirstName;
-        this.createdAt = createdAt;
-        this.pk = pk;
-        this.text = text;
-    }
-
-    public Comment() {
-
-    }
-
-    public int getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(int author) {
-        this.author = author;
-    }
-
-    public String getAuthorImage() {
-        return authorImage;
-    }
-
-    public void setAuthorImage(String authorImage) {
-        this.authorImage = authorImage;
-    }
-
-    public String getAuthorFirstName() {
-        return authorFirstName;
-    }
-
-    public void setAuthorFirstName(String authorFirstName) {
-        this.authorFirstName = authorFirstName;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public int getPk() {
-        return pk;
-    }
-
-    public void setPk(int pk) {
-        this.pk = pk;
-    }
-
-    public String getText() {
-        return text;
-    }
-
-    public void setText(String text) {
-        this.text = text;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Comment comment = (Comment) o;
-        return author == comment.author && pk == comment.pk && Objects.equals(id, comment.id) && Objects.equals(authorImage, comment.authorImage) && Objects.equals(authorFirstName, comment.authorFirstName) && Objects.equals(createdAt, comment.createdAt) && Objects.equals(text, comment.text);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, author, authorImage, authorFirstName, createdAt, pk, text);
-    }
-
-    @Override
-    public String toString() {
-        return "Comment{" +
-                "id=" + id +
-                ", author=" + author +
-                ", authorImage='" + authorImage + '\'' +
-                ", authorFirstName='" + authorFirstName + '\'' +
-                ", createdAt=" + createdAt +
-                ", pk=" + pk +
-                ", text='" + text + '\'' +
-                '}';
-    }
 }
