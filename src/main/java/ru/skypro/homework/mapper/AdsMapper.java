e09c3d88fd167de601b1d2a0571267da193864a3
Marina Gubina <gubinamarina1998@gmail.com>
2023-05-30 15:52:57 +0300

    started doing AdsMapper

---
+++ src/main/java/ru/skypro/homework/mapper/AdsMapper.java
@@ -0,0 +1,33 @@
+package ru.skypro.homework.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
+import org.mapstruct.factory.Mappers;
+import ru.skypro.homework.dto.AdsDTO;
+import ru.skypro.homework.dto.CreateAdsDTO;
+import ru.skypro.homework.dto.FullAdsDto;
+import ru.skypro.homework.entity.Ad;
+
+import java.util.Collection;
+
+@Mapper(componentModel = "spring",unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface AdsMapper {
+
+    AdsMapper INSTANCE = Mappers.getMapper(AdsMapper.class);
+
+    @Mapping(source = "id",target = "pk")
+    @Mapping(source = "author.id",target = "author")
+    AdsDTO adToAdsDTO(Ad ad);
+
+    Ad adsDtoToAd(CreateAdsDTO createAdsDTO);
+
+    @Mapping(source = "id",target = "pk")
+    @Mapping(source = "author.firstName",target = "authorFirstName")
+    @Mapping(source = "author.lastName",target = "authorLastName")
+    @Mapping(source = "author.username",target = "email")
+    @Mapping(source = "author.phone",target = "phone")
+    FullAdsDto adToFullAdsDto(Ad ad);
+
+    Collection<AdsDTO> adsToAdsListDto(Collection<Ad> adsCollection);
+}
