41fb38875b4b4ec30dc2ac0120c235087bc87338
Marina Gubina <gubinamarina1998@gmail.com>
2023-06-02 13:22:34 +0300

    editing Ads classes

--- src/main/java/ru/skypro/homework/service/AdService.java
+++ src/main/java/ru/skypro/homework/service/AdService.java
@@ -1,14 +1,20 @@
 package ru.skypro.homework.service;
 
 
+import ru.skypro.homework.dto.AdsDTO;
+import ru.skypro.homework.dto.CreateAdsDTO;
 import ru.skypro.homework.dto.FullAdsDto;
 import ru.skypro.homework.dto.ResponseWrapperAdsDto;
+import ru.skypro.homework.entity.Image;
 
 import java.util.Collection;
 
 public interface AdService {
-   Collection<ResponseWrapperAdsDto> getAllAds();
-   FullAdsDto getFullAd(Integer adsId);
-   void deleteAd(Integer adsId);
-
+   Collection<AdsDTO> getAllAds(String title);
+   AdsDTO createAd(CreateAdsDTO createAdsDTO, Image image);
+   FullAdsDto getFullAd(long adId);
+   void deleteAd(long adId);
+   AdsDTO updateAd(CreateAdsDTO createAdsDTO, long adId);
+   Collection<AdsDTO> getUserAllAds(long userId);
+   String updateImage(long adId,Image image);
 }
--- src/main/java/ru/skypro/homework/service/impl/AdServiceImpl.java
+++ src/main/java/ru/skypro/homework/service/impl/AdServiceImpl.java
@@ -1,26 +1,72 @@
 package ru.skypro.homework.service.impl;
 
 import org.springframework.stereotype.Service;
+import ru.skypro.homework.dto.AdsDTO;
+import ru.skypro.homework.dto.CreateAdsDTO;
 import ru.skypro.homework.dto.FullAdsDto;
-import ru.skypro.homework.dto.ResponseWrapperAdsDto;
+import ru.skypro.homework.entity.Ad;
+import ru.skypro.homework.entity.Image;
+import ru.skypro.homework.exception.AdNotFoundException;
+import ru.skypro.homework.mapper.AdsMapper;
+import ru.skypro.homework.repository.AdRepository;
 import ru.skypro.homework.service.AdService;
 
 import java.util.Collection;
 
 @Service
 public class AdServiceImpl implements AdService {
+
+    private final AdRepository adRepository;
+
+    public AdServiceImpl(AdRepository adRepository) {
+        this.adRepository = adRepository;
+    }
+
     @Override
-    public Collection<ResponseWrapperAdsDto> getAllAds() {
+    public Collection<AdsDTO> getAllAds(String title) {
+        if (title == null){
+            return null;
+        }
+
+        return AdsMapper.INSTANCE.adsToAdsListDto(
+                adRepository.findByTitleLikeIgnoreCase(title));
+    }
+
+    @Override
+    public AdsDTO createAd(CreateAdsDTO createAdsDTO, Image image) {
+        if(createAdsDTO == null){
+            return null;
+        }
+        Ad ad = AdsMapper.INSTANCE.adsDtoToAd(createAdsDTO);
+        ad.setImage(image);
+        adRepository.save(ad);
+
+        return AdsMapper.INSTANCE.adToAdsDTO(ad);
+    }
+
+    @Override
+    public FullAdsDto getFullAd(long adId) {
+        return AdsMapper.INSTANCE.adToFullAdsDto(
+                adRepository.findById(adId).orElseThrow(AdNotFoundException::new));
+    }
+
+    @Override
+    public void deleteAd(long adId) {
+        adRepository.deleteById(adId);
+    }
+
+    @Override
+    public AdsDTO updateAd(CreateAdsDTO createAdsDTO, long adId) {
         return null;
     }
 
     @Override
-    public FullAdsDto getFullAd(Integer adsId) {
+    public Collection<AdsDTO> getUserAllAds(long userId) {
         return null;
     }
 
     @Override
-    public void deleteAd(Integer adsId) {
-
+    public String updateImage(long adId, Image image) {
+        return null;
     }
 }
