90fb0c6e1c5729a1f8d5e5cf1bed1c12487a53e8
Alex <alexsa-8@mail.ru>
2023-06-19 21:54:24 +0300

    made a javaDoc to AuthServiceImpl.

--- src/main/java/ru/skypro/homework/service/impl/AuthServiceImpl.java
+++ src/main/java/ru/skypro/homework/service/impl/AuthServiceImpl.java
@@ -12,6 +12,15 @@ import ru.skypro.homework.mapper.UserMapper;
 import ru.skypro.homework.repository.UserRepository;
 import ru.skypro.homework.service.AuthService;
 
+/**
+ * Servic AuthServiceImpl
+ * Service for entering a name and password
+ * @see UserDetailsManager
+ * @see PasswordEncoder
+ * @see UserRepository
+ * @see UserMapper
+ * @author Kilikova Anna
+ */
 @Service
 @RequiredArgsConstructor
 public class AuthServiceImpl implements AuthService {
@@ -24,13 +33,27 @@ public class AuthServiceImpl implements AuthService {
 
   private final UserMapper userMapper;
 
-
+  /**
+   * The method returns the username used for authentication
+   *
+   * @param userName user name
+   * @param password user password
+   * @return returns the user's password
+   */
   @Override
   public boolean login(String userName, String password) {
     UserDetails userDetails = manager.loadUserByUsername(userName);
     return encoder.matches(password, userDetails.getPassword());
   }
 
+  /**
+   * The method determines whether the user's value is present in the register
+   *
+   * @param registerReq registration
+   * @param role role
+   * @return false - if the value is missing
+   * @return true - if the value is present
+   */
   @Override
   public boolean register(RegisterReq registerReq, Role role) {
     if (userRepository.findByEmailIgnoreCase(registerReq.getUsername()).isPresent()) {
